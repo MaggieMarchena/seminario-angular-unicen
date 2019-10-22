9605facaa6d85f6a99f47165984237ba475c3cbf
Magali Molina <v-magmolina@expedia.com>
2019-10-22 18:26:50 -0300

    Changes how stock is shown

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerListComponent implements OnInit {
 
+  public beers: any = [
+    {
+      name: 'Black Bear',
+      style: 'Porter',
+      price: 130,
+      image: '',
+      inStock: 5
+    },
+    {
+      name: 'Sunny',
+      style: 'Golden',
+      price: 110,
+      image: '',
+      inStock: 10
+    },
+    {
+      name: 'Fire',
+      style: 'Red',
+      price: 120,
+      image: '',
+      inStock: 0
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
