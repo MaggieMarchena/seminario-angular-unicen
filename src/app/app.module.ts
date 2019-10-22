b6282b1290be61737640fbba1af1640318c0f5da
Magali Molina <v-magmolina@expedia.com>
2019-10-22 17:20:55 -0300

    Adds components and basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
+import { NavComponent } from './nav/nav.component';
+import { CartComponent } from './cart/cart.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerListComponent,
+    NavComponent,
+    CartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
